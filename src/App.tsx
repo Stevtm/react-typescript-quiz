0d0e641eeb6539b8520a1388b29db3481f5b0faf
Stevtm <steventmaddison@outlook.com>
2021-08-06 08:27:36 -0400

    scaffold App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+// component imports
+import QuestionCard from "./components/QuestionCard";
 
-function App() {
-	return <div className="App">Quiz</div>;
-}
+const App = () => {
+	const startQuiz = async () => {};
+
+	const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+	const nextQuestion = () => {};
+
+	return (
+		<div className="App">
+			<h1>React Quiz</h1>
+			<button className="start" onClick={startQuiz}>
+				Start
+			</button>
+			<p className="score">Score:</p>
+			<p>Loading Questions...</p>
+			<QuestionCard></QuestionCard>
+			<button className="next" onClick={nextQuestion}>
+				Next Questions
+			</button>
+		</div>
+	);
+};
 
 export default App;
